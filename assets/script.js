49159e057d11c8e9159edb81e2c38ac393279a61
Shinichi <miyakawashi@gmail.com>
2021-12-02 17:34:07 -0500

    final files

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,5 @@
+var tasks =[];
+
 // Gets current date and formats to DayOfWeek, Month Date(#th) 
 var getCurrentDate = function () {
     var currentDateTime = new Date();
@@ -61,13 +63,80 @@ var scheduleTimeCompare = function () {
     scheduleTimeArr.each(function(){
         var timeOfDay = formatAmPm($(this).text());
         if(parseInt(currentHour) > parseInt(timeOfDay)) {
-            $(this).siblings("#task").addClass("bg-secondary")
-        } else {
-            $(this).siblings("#task").addClass("bg-success bg-gradient")
+            $(this).siblings("#task").addClass("past")
+        }  else if (parseInt(currentHour) === parseInt(timeOfDay)) {
+            $(this).siblings("#task").addClass("present");
+        }
+        else {
+            $(this).siblings("#task").addClass("future");
         }
     });
 };
 
+// save tasks to local storage
+var saveTask = function() {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+};
+
+// Load previous stored tasks from local storage, if no storage available
+// load default values in tasks array.
+var loadTask = function() {
+    // get task items from local storage
+    parsedTask = JSON.parse(localStorage.getItem("tasks"));
+
+    if (!parsedTask) {
+        $('*textarea').each(function() {
+            tasks.push(
+            $(this).val().trim()
+            );
+        });
+        saveTask(tasks);
+    } else {
+        // update tasks array to hold stored tasks
+        for (let i = 0; i < parsedTask.length; i++) {
+            const element = parsedTask[i];
+            tasks.push(element);
+
+            $("*textarea").each(function(){
+                console.log($(this).closest(".row").index())
+                if($(this).closest(".row").index() === i) {
+                    $(this).val(element);
+                }
+            });  
+        }
+    }
+};
+
+// get eventlistener for text area
+$("textarea").on("click", function() {
+    $(this).trigger("focus");
+    console.log("task update");
+});
+
+// listen to off click on text area then update tasks array
+$("textarea").on("blur", function() {
+    console.log("is this even working");
+    var text = $(this)
+    .val();
+    
+    var index = $(this)
+    .closest(".row")
+    .index();
+
+    tasks[index] = text;
+});
+
+// listen to save clicks from user, then save new task array
+$("button").on("click", function(){
+    console.log("Clicked Save")
+    saveTask(tasks);
+});
+
+// get eventlistener for save button
+// save text area task to local storage
+
+
+loadTask();
 setCurrentDate();
 getCurrentHour();
 scheduleTimeCompare();
